6e4c49b305e7488fdadde47d14bdcb7a0d491658
bruno.bozzetti <bruno.bozzetti@dbserver.com.br>
2023-03-16 09:11:05 -0300

    created a custom theme for types

--- app/Pokemon/index.js
+++ app/Pokemon/index.js
@@ -3,16 +3,20 @@ import { useRoute } from "@react-navigation/native";
 import CompPokemonHeader from "../../components/CompPokemonHeader";
 import { StatusBar } from "react-native";
 import { getPokemonInfo } from "../../graphql/GetPokemonInfo";
+import { useTheme } from "@ui-kitten/components";
 
 export default function Pokemon() {
   const route = useRoute();
   const { id } = route.params;
   const [pokemon, setPokemon] = useState(null);
+  const [type, setType] = useState("");
+  const theme = useTheme();
 
-  // get the list
+  // get the pokemon by ID
   useEffect(() => {
     getPokemonInfo(id).then((data) => {
       setPokemon(data);
+      setType(data.pokemon_v2_pokemontypes[0].pokemon_v2_type.name);
     });
   }, [id]);
 
@@ -22,12 +26,13 @@ export default function Pokemon() {
         <StatusBar
           barStyle="light-content"
           hidden={false}
-          backgroundColor="#900"
+          backgroundColor={theme["type-" + type]}
           translucent={false}
         />
         <CompPokemonHeader
           id={pokemon.id}
           name={pokemon.name}
+          type={type}
         ></CompPokemonHeader>
       </>
     );
--- components/CompPokemonHeader.js
+++ components/CompPokemonHeader.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-import { Layout, Text, Icon, Button } from "@ui-kitten/components/ui";
+import { Layout, Text, Icon } from "@ui-kitten/components/ui";
 import { useRouter } from "expo-router";
+import { useTheme } from "@ui-kitten/components";
 
 export default function CompPokemonHeader(props) {
   const router = useRouter();
+  const theme = useTheme();
 
   return (
-    <Layout style={styles.header}>
+    <Layout
+      style={[styles.header, { backgroundColor: theme["type-" + props.type] }]}
+    >
       <Text style={styles.text} category="h6">
         {props.id} - {props.name}
       </Text>
@@ -26,7 +30,6 @@ export default function CompPokemonHeader(props) {
 const styles = StyleSheet.create({
   header: {
     height: 70,
-    backgroundColor: "#900",
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
