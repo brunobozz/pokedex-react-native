9b057487f89635fd288524d455167d9996197493
bruno.bozzetti <bruno.bozzetti@dbserver.com.br>
2023-03-20 09:41:16 -0300

    added types on pokemonList

--- components/CompPokemonList.js
+++ components/CompPokemonList.js
@@ -1,6 +1,7 @@
-import { Divider, List, ListItem, Text } from "@ui-kitten/components";
+import { Divider, Layout, List, ListItem, Text } from "@ui-kitten/components";
+import { StyleSheet } from "react-native";
 import { useRouter } from "expo-router";
-import { uppercase } from "graphql-request/build/esm/helpers";
+import CompCircleType from "./CompCircleType";
 
 export default function CompPokemonList(props) {
   const router = useRouter();
@@ -12,6 +13,7 @@ export default function CompPokemonList(props) {
       renderItem={({ item }) => (
         <ListItem
           key={item.id}
+          style={styles.listItem}
           onPress={() => {
             router.push("/Pokemon?id=" + item.id);
           }}
@@ -19,8 +21,38 @@ export default function CompPokemonList(props) {
           <Text
             style={{ textTransform: "capitalize", fontSize: 16 }}
           >{`${item.id} - ${item.name}`}</Text>
+
+          <Layout style={styles.types}>
+            {item.pokemon_v2_pokemontypes.map((type) => (
+              <Layout style={styles.circle}>
+                <CompCircleType
+                  type={type.pokemon_v2_type.name}
+                ></CompCircleType>
+              </Layout>
+            ))}
+          </Layout>
         </ListItem>
       )}
     />
   );
 }
+
+const styles = StyleSheet.create({
+  listItem: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  types: {
+    backgroundColor: "transparent",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  circle: {
+    backgroundColor: "transparent",
+    marginLeft: 5,
+  },
+});
